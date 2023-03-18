0ee5d401657ed0476e350996aae3ffff6bc6c7cc
camilolcoder <j.colmenares@uniandes.edu.co>
2023-03-18 11:38:08 -0500

    cambnios test

--- widmy/historiaClinica/urls.py
+++ widmy/historiaClinica/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
         path('hclinicas/', views.hclinica_list),
+        path('activas',views.activas, name='activas'), 
     ]
     #path('', views.historiasclinicas_view, name='historiasclinicas_view'),
     #path('<int:pk>', views.historiaclinica_view, name='historiaclinica_view'),
